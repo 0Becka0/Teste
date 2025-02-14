dcfa8576c0da22f3a7df8d6a429f5649287f0dc1
Ruan de Albuquerque santos <ruan.albuquerque@appc.com>
2025-02-14 17:17:25 -0300

    Removendo o caminho do icone da notificacao, adicionando texto no readme e adicionando o ambiente virtual

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Teste
+# Repositório Teste
+
+Esse repositório foi criado unicamente com a idéia de por em prática comandos do git e github.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
-toaster.show_toast("Título da Notificação", "Aqui está a mensagem!", duration=10, icon_path="\\\\192.168.0.62\\Cobrança\\Dependency\\Image\\notificacao_padrao.ico")
+toaster.show_toast("Título da Notificação", "Aqui está a mensagem!", duration=10)
